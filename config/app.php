5df8edb83957acff700bc3557a83a62396cfbed2
sonuprasad2023 <sonu@kawikatechnologies.com>
2024-03-27 10:48:23 +0530

    files

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
         //App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Jenssegers\Mongodb\MongodbServiceProvider::class,
+       // Jenssegers\Mongodb\MongodbServiceProvider::class,
         Mews\Captcha\CaptchaServiceProvider::class,
 
 
